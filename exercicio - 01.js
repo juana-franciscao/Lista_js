f2039498873decaa275dae24687b3d44e2a490c6
Juana-Franciscao-2213 <juana_franciscao@estudante.sesisenai.org.br>
2026-03-17 08:49:22 -0300

    add- iniciei e terminei a atividade - 01

---
+++ exercicio - 01.js
@@ -0,0 +1,21 @@
+function somarNumero(){
+    let soma = 0;
+    let continuar = true;
+
+    while( continuar ) {
+        let numero = parseFloat(prompt("Didite um número (ou digite '0' para encerrar):"));
+
+        if(!isNaN(numero)){
+
+            soma+=numero;
+
+        }else{
+            alert("Por favor, digite  um número válido.");
+        }
+
+        continuar = confirm("Deseja continuar a adicionar mais números?");
+    }
+    alert("Soma dos números é:" + soma)
+}
+
+somarNumero()
