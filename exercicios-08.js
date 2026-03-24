71850477e4ba5af832b13fc496ac4bcd004af5c9
Juana-Franciscao-2213 <juana_franciscao@estudante.sesisenai.org.br>
2026-03-24 09:21:11 -0300

    up-criei e finalizei o exercicio-8, arrumei os botões

---
+++ exercicios-08.js
@@ -0,0 +1,9 @@
+function duasVariaveis() {
+
+    let n1 = prompt("Digite o primeiro número: ");
+    let n2 = prompt("Digite o segundo número: ");
+
+    console.log(n1 - n2);
+    console.log((2*n1) + (3*n2));
+    console.log(n1 * n2);
+}
