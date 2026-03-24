d4bbfd39b8225453facbea82e77af972d751e203
Juana-Franciscao-2213 <juana_franciscao@estudante.sesisenai.org.br>
2026-03-24 07:41:34 -0300

    add - criei e finalizei a atividade 6, adicionei um botão pra atividade 6

---
+++ exercicio-06.js
@@ -0,0 +1,13 @@
+function vogalConsoante() {
+    let vogal = 0;
+    let consoante = 0;
+
+    let letra = prompt("Digite um numero inteiro de 1 a 10 ");
+
+    if (letra == "a"||letra == "e"||letra == "i"||letra == "o"||letra == "u") {
+    console.log("Essa letra é uma vogal")
+      
+    } else {
+        console.log("Essa letre é uma vogal");
+    }
+}
