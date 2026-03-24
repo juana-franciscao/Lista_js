ba4d93ad655474f2dbdcfc0c82f8fc27de3f7fdd
Juana-Franciscao-2213 <juana_franciscao@estudante.sesisenai.org.br>
2026-03-24 09:01:25 -0300

    up-atereçoes na atividade 6, iniciei e finalizei atividade 07

--- exercicio-06.js
+++ exercicio-06.js
@@ -1,13 +1,10 @@
 function vogalConsoante() {
-    let vogal = 0;
-    let consoante = 0;
+    let letra = prompt("Digite uma letra do afabeto:");
 
-    let letra = prompt("Digite um numero inteiro de 1 a 10 ");
+    if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
+        console.log("Essa letra é uma vogal")
 
-    if (letra == "a"||letra == "e"||letra == "i"||letra == "o"||letra == "u") {
-    console.log("Essa letra é uma vogal")
-      
     } else {
-        console.log("Essa letre é uma vogal");
+        console.log("Essa letra é uma consoante");
     }
 }
---
+++ exercicio-07.js
@@ -0,0 +1,28 @@
+function pecolePreco() {
+
+    alert("Escolha entre as opções: \n (a) Chocolate\n (b) Morango;\n (c) Creme\n (d) Manga\n (e) Melancia\n (f) Vanilla Ice\n (g) Céu Azul\n (h) Brownie\n (i) Hawaiano\n");
+    let sabor = prompt("Digite a letra correspondente para cada sabor:");
+
+
+    if (sabor == "a") {
+        alert("Chocolate-R$ 1,50")
+    } else if (sabor == "b") {
+        alert("Morango-R$ 2,50")
+    } else if (sabor == "c") {
+        alert("Creme-R$ 2,50")
+    } else if (sabor == "d") {
+        alert("Manga-R$ 3,20")
+    } else if (sabor == "e") {
+        alert("Melancia-R$ 3,40")
+    } else if (sabor == "f") {
+        alert("Vanilla Ice-R$ 3,00")
+    } else if (sabor == "g") {
+        alert("Céu Azul-R$ 3,60")
+    } else if (sabor == "h") {
+        alert("Brownie-R$ 4,00")
+    } else if (sabor == "i") {
+        alert("Hawaiano-R$ 5,00")
+    }
+
+}
+
