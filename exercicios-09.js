6ebe0a8045f9deed6828044c76768ad48564600c
Juana-Franciscao-2213 <juana_franciscao@estudante.sesisenai.org.br>
2026-03-24 09:38:40 -0300

    add - criei e finalixei exrecicios-09

---
+++ exercicios-09.js
@@ -0,0 +1,12 @@
+function maiorMenor() {
+
+    let n1 = prompt("Digite o primeiro número: ");
+    let n2 = prompt("Digite o segundo número: ");
+
+    if(n1<n2){
+        console.log(n1, n2);
+    }else{
+       console.log(n2, n1); 
+    }
+    
+}
